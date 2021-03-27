7ba13c0a5be4c4fed7d223b7587ef2e0e9c71019
bouboumael <bouboumael@gmail.com>
2021-03-27 18:06:26 +0100

    first commit

---
+++ Car.php
@@ -0,0 +1,116 @@
+<?php
+
+class Car
+{
+
+    private int $numberWheels;
+    private int $currentSpeed;
+    private string $color;
+    private int $numberSeats;
+    private string $energy;
+    private int $energyLevel;
+
+    public function __construct(
+        int $numberWheels,
+        int $currentSpeed,
+        string $color,
+        int $numberSeats,
+        string $energy,
+        int $energyLevel)
+    {
+        $this->numberWheels = $numberWheels;
+        $this->currentSpeed = $currentSpeed;
+        $this->color = $color;
+        $this->numberSeats = $numberSeats;
+        $this->energy = $energy;
+        $this->energyLevel = $energyLevel;
+    }
+    
+    public function getNumberWheels()
+    {
+        return $this->numberWheels;
+    }
+
+    public function setNumberWheels($numberWheels)
+    {
+        $this->numberWheels = $numberWheels;
+
+        return $this;
+    }
+
+    public function getCurrentSpeed()
+    {
+        return $this->currentSpeed;
+    }
+
+    public function setCurrentSpeed($currentSpeed)
+    {
+        $this->currentSpeed = $currentSpeed;
+
+        return $this;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+ 
+    public function setColor($color)
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getNumberSeats()
+    {
+        return $this->numberSeats;
+    }
+
+    public function setNumberSeats($numberSeats)
+    {
+        $this->numberSeats = $numberSeats;
+
+        return $this;
+    }
+
+    public function getEnergy()
+    {
+        return $this->energy;
+    }
+
+    public function setEnergy($energy)
+    {
+        $this->energy = $energy;
+
+        return $this;
+    }
+
+    public function getEnergyLevel()
+    {
+        return $this->energyLevel;
+    }
+
+    public function setEnergyLevel($energyLevel)
+    {
+        $this->energyLevel = $energyLevel;
+
+        return $this;
+    }
+
+    public function start ()
+    {
+        
+    }
+
+    public function foward ()
+    {
+
+    }
+
+    public function brake ()
+    {
+
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'Car.php';
+
+$car = new Car(4, 15, 'red', 4, 'electic', 50);
+
+var_dump($car);
